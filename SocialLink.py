34ff1bf9f29db514268aad55ef8c2bb6004f5127
BasicallyKnight <84093031+BasicallyKnight@users.noreply.github.com>
2021-05-12 19:50:39 -0700

    Add files via upload

---
+++ SocialLink.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+from datetime import datetime
+from termcolor import colored
+
+cluster = MongoClient("mongodb+srv://DC:coding@sociallink.trzdk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = cluster["SocialLink"]["test run"]
+all = db.find({})
+date = datetime.now().strftime("%x")
+
+for messages in all:
+    try:
+        if date != messages["date"]:
+            print(colored(f"Today - {messages['time']}", 'red'))
+        else:
+            print(colored(f"{messages['date']} - {messages['time']}", 'red'))
+        print(colored("From ", 'green'), messages['id'])
+        print(colored("Messages: ", 'green'), messages['messages'])
+        print("_________")
+    except:
+        pass
+    
+person = input("Name: ")
+message = input("Message: ")
+
+time = datetime.now().strftime("%x")
+msg = {"id": person, "message": message, "date": date, "time": time}
+
+db.insert_one(msg)
